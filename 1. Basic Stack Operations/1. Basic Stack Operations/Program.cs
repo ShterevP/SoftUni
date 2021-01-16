95ac6e0b8c7ecb3708e84462fc4fff3068eeae99
Panayot Shterev <p.shterev1@gmail.com>
2021-01-16 17:18:26 +0200

    last version

--- 1. Basic Stack Operations/1. Basic Stack Operations/Program.cs
+++ 1. Basic Stack Operations/1. Basic Stack Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _1._Basic_Stack_Operations
 {
@@ -6,7 +8,43 @@ namespace _1._Basic_Stack_Operations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> numStack = new Stack<int>();
+
+            string[] input1 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int n = int.Parse(input1[0]);
+            int s = int.Parse(input1[1]);
+            int x = int.Parse(input1[2]);
+
+            string[] input2 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < n; i++)
+            {
+                numStack.Push(int.Parse(input2[i]));
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                numStack.Pop();
+            }
+
+            if (numStack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else if(numStack.Count > 0)
+            {
+                int smallestNum = numStack.Min();
+
+                Console.WriteLine(smallestNum);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
         }
     }
 }
