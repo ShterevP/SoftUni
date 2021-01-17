4c2c815bebd5be93a666a7dbdaf197d9c006a759
Panayot Shterev <p.shterev1@gmail.com>
2021-01-17 14:46:33 +0200

    todays commit

--- 3. Maximum and Minimum Element/3. Maximum and Minimum Element/Program.cs
+++ 3. Maximum and Minimum Element/3. Maximum and Minimum Element/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _3._Maximum_and_Minimum_Element
 {
@@ -6,7 +8,51 @@ namespace _3._Maximum_and_Minimum_Element
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            Stack<int> numbers = new Stack<int>();
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int[] tokens = input
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                switch (tokens[0])
+                {
+                    case 1:
+                        int elementToPush = tokens[1];
+                        numbers.Push(elementToPush);
+                        break;
+
+                    case 2:
+                        if (numbers.Count > 0)
+                        {
+                            numbers.Pop();
+                        }
+                        break;
+                    case 3:
+                        if (numbers.Count > 0)
+                        { 
+                            Console.WriteLine(numbers.Max());
+                        }
+
+                        break;
+                    case 4:
+                        if (numbers.Count > 0)
+                        {
+                            Console.WriteLine(numbers.Min());
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
         }
     }
 }
