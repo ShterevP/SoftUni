886a23efc9ca1b89d43f3a3aadce5487eaf53eb8
Panayot Shterev <p.shterev1@gmail.com>
2021-01-17 18:05:26 +0200

    18:05

---
+++ 5. Fashion Boutique/5. Fashion Boutique/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _5._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesValues = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> clothesStack = new Stack<int>();
+
+            foreach (var clothValue in clothesValues)
+            {
+                clothesStack.Push(clothValue);
+            }
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int racksCount = 0;
+            int currentRackCapacity = rackCapacity;
+
+            while (clothesStack.Count > 0)
+            {
+                if (clothesStack.Peek() > currentRackCapacity)
+                {
+                    racksCount++;
+                    currentRackCapacity = rackCapacity - clothesStack.Pop();
+                }
+                else
+                {
+                    currentRackCapacity -= clothesStack.Pop();
+                    if (racksCount == 0)
+                    {
+                        racksCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine(racksCount);
+        }
+    }
+}
