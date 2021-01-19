466138412c02f578c8d7aa1bbe56173bbba830c4
Panayot Shterev <p.shterev1@gmail.com>
2021-01-19 20:06:37 +0200

    20:06

---
+++ Stacks and Queues/8. Balanced Parentheses/8. Balanced Parentheses/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8._Balanced_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Queue<char> parentheses = new Queue<char>(input);
+
+            bool squareBracketOpen = false;
+            bool curlyBracketOpen = false;
+            bool roundBracketOpen = false;
+            bool spaceOpen = false;
+
+            foreach (var item in parentheses)
+            {
+                switch (item)
+                {
+                    case '{':
+                        curlyBracketOpen = true;
+                        break;
+
+                    case '}':
+                        if (curlyBracketOpen)
+                        {
+                            curlyBracketOpen = false;
+                        }
+                        break;
+
+                    case '(':
+                        roundBracketOpen = true;
+                        break;
+
+                    case ')':
+                        if (roundBracketOpen)
+                        {
+                            roundBracketOpen = false;
+                        }
+                        break;
+
+                    case '[':
+                        squareBracketOpen = true;
+                        break;
+
+                    case ']':
+                        if (squareBracketOpen)
+                        {
+                            squareBracketOpen = false;
+                        }
+                        break;
+
+                    case ' ':
+                        if (spaceOpen)
+                        {
+                            spaceOpen = false;
+                        }
+                        else
+                        {
+                            spaceOpen = true;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            bool isBalanced = !(squareBracketOpen
+                || roundBracketOpen 
+                || curlyBracketOpen
+                || spaceOpen);
+
+            Console.WriteLine(isBalanced?"YES":"NO");
+        }
+    }
+}
