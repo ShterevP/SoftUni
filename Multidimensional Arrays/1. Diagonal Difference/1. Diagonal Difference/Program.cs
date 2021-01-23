6d265ddb796cf1872db5cf988e9e4ab0ecb873de
Panayot Shterev <p.shterev1@gmail.com>
2021-01-23 13:34:31 +0200

    13:34

--- Multidimensional Arrays/1. Diagonal Difference/1. Diagonal Difference/Program.cs
+++ Multidimensional Arrays/1. Diagonal Difference/1. Diagonal Difference/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _1._Diagonal_Difference
 {
@@ -6,7 +7,30 @@ namespace _1._Diagonal_Difference
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[][] squareMatrix = new int[n][];
+            int primaryDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                squareMatrix[row] = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                primaryDiagonalSum += squareMatrix[i][i];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                secondaryDiagonalSum += squareMatrix[i][n - 1 - i];
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
         }
     }
 }
