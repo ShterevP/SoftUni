65db72b35cef16c27940717482f5f143d60051d0
Panayot Shterev <p.shterev1@gmail.com>
2021-01-23 13:35:30 +0200

    13:35

---
+++ Multidimensional Arrays/2. 2X2 Squares in Matrix/2X2 Squares in Matrix/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _2X2_Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsCols = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = rowsCols[0];
+            int cols = rowsCols[1];
+            int equal2x2Count = 0;
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] rowData = Console.ReadLine().Split(" ").ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            for (int col = 0; col < cols - 1; col++)
+            {
+                for (int row = 0; row < rows - 1; row++)
+                {
+                    if (matrix[row, col] == matrix[row + 1,col] 
+                        && matrix[row + 1, col] == matrix[row, col +1]
+                        && matrix[row, col + 1] == matrix[row + 1, col + 1])
+                    {
+                        equal2x2Count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(equal2x2Count);
+
+        }
+    }
+}
---
+++ Multidimensional Arrays/3. Maximal Sum/3. Maximal Sum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Maximal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsCols = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = rowsCols[0];
+            int cols = rowsCols[1];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+
+                }
+            }
+        }
+    }
+}
