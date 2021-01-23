f61524d9f9d6be3044b08fbda8367a3a2f8331d5
Panayot Shterev <p.shterev1@gmail.com>
2021-01-23 19:10:09 +0200

    19:10

--- Multidimensional Arrays/3. Maximal Sum/3. Maximal Sum/Program.cs
+++ Multidimensional Arrays/3. Maximal Sum/3. Maximal Sum/Program.cs
@@ -14,14 +14,62 @@ namespace _3._Maximal_Sum
 
             int rows = rowsCols[0];
             int cols = rowsCols[1];
+            int sum = 0;
+            int maxSum = 0;
+            int maxSumRowIndex = 0;
+            int maxSumColIndex = 0;
+
+            int[,] matrix = new int[rows, cols];
 
             for (int row = 0; row < rows; row++)
             {
+                int[] rowData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
                 for (int col = 0; col < cols; col++)
                 {
-
+                    matrix[row, col] = rowData[col];
                 }
             }
+
+            for (int row = 0; row < rows - 2; row++)
+            {
+                for (int col = 0; col < cols - 2; col++)
+                {
+                    sum = matrix[row, col];
+                    sum += matrix[row, col + 1];
+                    sum += matrix[row, col + 2];
+
+                    sum += matrix[row + 1, col];
+                    sum += matrix[row + 1, col + 1];
+                    sum += matrix[row + 1, col + 2];
+
+                    sum += matrix[row + 2, col];
+                    sum += matrix[row + 2, col + 1];
+                    sum += matrix[row + 2, col + 2];
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxSumRowIndex = row;
+                        maxSumColIndex = col;
+                    }
+                }
+
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+            for (int row = maxSumRowIndex; row < maxSumRowIndex + 3 ; row++)
+            {
+                for (int col = maxSumColIndex; col < maxSumColIndex + 3; col++)
+                {
+                    Console.Write($"{matrix[row,col]} ");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
---
+++ Multidimensional Arrays/4. Matrix Shuffling/4. Matrix Shuffling/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _4._Matrix_Shuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsCols = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = rowsCols[0];
+            int cols = rowsCols[1];
+            int rowFromCoordinate = 0;
+            int colFromCoordinate = 0;
+            int rowToCoordinate = 0;
+            int colToCoordinate = 0;
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] rowdata = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowdata[col];
+                }
+            }
+            string input = Console.ReadLine();
+            
+            while (input != "END")
+            {
+                string[] tokens = input
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+                if (tokens.Length == 5
+                    && tokens[0] == "swap"
+                    && int.TryParse(tokens[1], out rowFromCoordinate)
+                    && int.TryParse(tokens[2], out colFromCoordinate)
+                    && int.TryParse(tokens[3], out rowToCoordinate)
+                    && int.TryParse(tokens[4], out colToCoordinate))
+
+                {
+                    if (rowFromCoordinate <= matrix.GetLength(0) - 1
+                        && rowToCoordinate <= matrix.GetLength(0) -1
+                        && colFromCoordinate <= matrix.GetLength(1) - 1
+                        && colToCoordinate <= matrix.GetLength(1) - 1)
+                    {
+                        string temp = matrix[rowToCoordinate, colToCoordinate];
+                        matrix[rowToCoordinate, colToCoordinate] = matrix[rowFromCoordinate,colFromCoordinate];
+                        matrix[rowFromCoordinate, colFromCoordinate] = temp;
+                        matrixPrint(matrix);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+                input = Console.ReadLine();
+            }
+            
+
+
+        }
+
+        private static void matrixPrint(string[,] matrixInput)
+        {
+            for (int row = 0; row < matrixInput.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrixInput.GetLength(1); col++)
+                {
+                    Console.Write($"{matrixInput[row, col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
