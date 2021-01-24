9cfb91992d8f44db08b944581b3c3592f0a0d1ac
Panayot Shterev <p.shterev1@gmail.com>
2021-01-24 19:48:02 +0200

    19:47

--- Multidimensional Arrays/5. Snake Moves/5. Snake Moves/Program.cs
+++ Multidimensional Arrays/5. Snake Moves/5. Snake Moves/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _5._Snake_Moves
 {
@@ -6,7 +7,36 @@ namespace _5._Snake_Moves
     {
         static void Main(string[] args)
         {
-            
+            int[] inputNandM = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = inputNandM[0];
+            int cols = inputNandM[1];
+
+            string[,] isleMatrix = new string[rows, cols];
+
+            string snake = Console.ReadLine();
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+
+                    }
+                }
+                else
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+
+                    }
+                }
+
+            }
         }
     }
 }
---
+++ Multidimensional Arrays/6. Jagged Array Manipulator/6. Jagged Array Manipulator/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+
+namespace _6._Jagged_Array_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfRows = int.Parse(Console.ReadLine());
+            double[][] matrix = new double[numOfRows][];
+
+            for (int row = 0; row < numOfRows; row++)
+            {
+                double[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(double.Parse)
+                    .ToArray();
+
+                matrix[row] = new double[input.Length];
+
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    matrix[row][col] = input[col];
+                }
+            }
+
+            for (int row = 0; row < numOfRows - 1; row++)
+            {
+                if (matrix[row].Length == matrix[row + 1].Length)
+                {
+                    for (int col = 0; col < matrix[row].Length; col++)
+                    {
+                        matrix[row][col] *= 2;
+                        matrix[row + 1][col] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int currentRow = row; currentRow <= row + 1; currentRow++)
+                    {
+                        for (int col = 0; col < matrix[currentRow].Length; col++)
+                        {
+                            matrix[currentRow][col] /= 2;
+                        }
+                    }
+                }
+            }
+
+            string inputCommand = Console.ReadLine();
+
+            while (inputCommand != "End")
+            {
+                string[] tokens = inputCommand
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                switch (tokens[0])
+                {
+                    case "Add":
+                        int rowToAdd = int.Parse(tokens[1]);
+                        int colToAdd = int.Parse(tokens[2]);
+                        double valueToAdd = int.Parse(tokens[3]);
+
+                        if (rowToAdd >= 0 && colToAdd >= 0)
+                        {
+                            if (rowToAdd < matrix.GetLength(0) && colToAdd < matrix[rowToAdd].Length)
+                            {
+                                matrix[rowToAdd][colToAdd] += valueToAdd;
+                            }
+                        }
+                        break;
+
+                    case "Subtract":
+                        int rowToSubtract = int.Parse(tokens[1]);
+                        int colToSubtract = int.Parse(tokens[2]);
+                        double valueToSubtract = int.Parse(tokens[3]);
+
+                        if (rowToSubtract >= 0 && colToSubtract >= 0)
+                        {
+                            if (rowToSubtract < matrix.GetLength(0) && colToSubtract < matrix[rowToSubtract].Length)
+                            {
+                                matrix[rowToSubtract][colToSubtract] -= valueToSubtract;
+                            }
+                        }
+                            break;
+
+                    default:
+                        break;
+                }
+
+                inputCommand = Console.ReadLine();
+            }
+
+            for (int row = 0; row < numOfRows; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write($"{matrix[row][col]} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
